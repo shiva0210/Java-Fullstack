4ffc6e7076d45e9800567e319c29357de7d0e554
Shivam Zodge <shivamzodge1600@gmail.com>
2024-09-27 22:45:09 +0530

    code add

---
+++ Nested For Loop/p11.java
@@ -0,0 +1,18 @@
+class Demo{
+	public static void main(String[]args){
+	char ch;
+	for(ch= 'A';ch<='D';ch++){
+	for(int j=1;j<=4;j++){
+	System.out.print(ch+ " ");
+}
+	System.out.println();
+	
+}
+}
+}	
+
+O/P--
+A A A A
+B B B B
+C C C C
+D D D D
---
+++ Nested For Loop/p12.java
@@ -0,0 +1,17 @@
+class Demo{
+	public static void main(String[]args){
+	for(int i=1;i<=4;i++){
+	for(char ch='A';ch<='D';ch++){
+	System.out.print(ch+ " ");
+}
+	System.out.println();
+	
+}
+}
+}
+
+O/P--
+A B C D
+A B C D
+A B C D
+A B C D	
